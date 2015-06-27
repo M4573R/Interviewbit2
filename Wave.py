a1ab78195f017a7e48dba6a590300e2016618dac
M4573R <tasmiah.tahsin@hotmail.com>
2015-06-27 03:35:09 -0700

    Update and rename Wave to Wave.py

---
+++ Wave.py
@@ -0,0 +1,27 @@
+"""Given an array of integers, sort the array into a wave like array and return it, 
+namely a1 >= a2 <= a3 >= a4 <= a5.....
+
+Example
+
+Given [1, 2, 3, 4]
+
+One possible answer : [2, 1, 4, 3]
+nother possible answer : [4, 1, 3, 2]
+NOTE : If there are multiple answers possible, return the one thats lexicographically smallest. 
+So, in example case, you will return [2, 1, 4, 3] """
+class Solution:
+    # @param A : list of integers
+    # @return a list of integers
+    def wave(self, A):
+        
+        if(len(A)==1):
+            return A
+        else:
+            A.sort()
+            for i in xrange(0,len(A)-1,2):
+                A[i],A[i+1] = A[i+1],A[i]
+		
+	
+	
+	    return A
+	    
