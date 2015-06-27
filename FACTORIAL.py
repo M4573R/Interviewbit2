a5964acffd8f7ee1a56bb4040d4c6e7750d475ba
M4573R <tasmiah.tahsin@hotmail.com>
2015-06-27 07:01:48 -0700

    Create FACTORIAL.py

---
+++ FACTORIAL.py
@@ -0,0 +1,44 @@
+
+"""Given an integer n, return the number of trailing zeroes in n!.
+
+Note: Your solution should be in logarithmic time complexity."""
+
+
+class Solution:
+    # @param A : integer
+    # @return an integer
+    def trailingZeroes(self, A):
+        count = 0
+        if(A == 5):
+            return 1
+        j = 5
+        while(A/j>=1):
+            count+=A/j
+            j*=5
+        return count
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
